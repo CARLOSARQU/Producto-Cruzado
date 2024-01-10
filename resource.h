b12a08aae4ce942f9b7c0d338466e5efd630350f
CARLOSARQU <111257254+CARLOSARQU@users.noreply.github.com>
2024-01-09 23:00:02 -0500

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,14 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define DLG_MAIN                                100
+#define EDITX               200
+#define EDITY               201
+#define POSX                202
+#define EDITX2              203
+#define EDITY2              204
+#define POSX2               205
+#define INTER               206
+#define SUPER               207
+#define PARAL               208
